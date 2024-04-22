a220f924e229d6ccb106665d31400fdd17b9532a
Tim <tim@rabbies.com>
2024-04-22 11:53:46 +0100

    exercise done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Http\Middlewares\SecurityHeaders;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,8 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+       })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
